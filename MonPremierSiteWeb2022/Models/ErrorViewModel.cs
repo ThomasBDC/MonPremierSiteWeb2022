df09474b7d5e1038cdcda20f4fee731e7b21b601
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-05-10 12:29:39 +0200

    add all

---
+++ MonPremierSiteWeb2022/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MonPremierSiteWeb2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
