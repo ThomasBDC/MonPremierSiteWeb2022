97666af11308ad1f186707573d558aec31a0e458
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-05-11 17:56:08 +0200

    Ajout d'un controller et d'une vue

---
+++ MonPremierSiteWeb2022/Controllers/MVCController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonPremierSiteWeb2022.Controllers
+{
+    public class MVCController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Commment créer notre modèle ? 
+            //Et comment le passer à notre vue ? 
+
+            return View();
+        }
+    }
+}
