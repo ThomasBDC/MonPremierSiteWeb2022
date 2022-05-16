071ef43e59eef83c788cbbadc236c18da002dad3
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-05-16 12:44:14 +0200

    Ajout d'une vue

---
+++ MonPremierSiteWeb2022/Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonPremierSiteWeb2022.Models;
+
+namespace MonPremierSiteWeb2022.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Récupération des données en BDD (ici une liste) modèle de domaine
+            var tousMesEleves = StudentsDatas.allStudents;
+
+            //On transforme notre modèle de domaine en modèle de vue
+            var vm = new StudentsViewModel()
+            {
+                lstStudents = tousMesEleves
+            };
+
+            //On passe le viewModel à la vue
+            return View(vm);
+        }
+    }
+}
---
+++ MonPremierSiteWeb2022/Models/StudentsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace MonPremierSiteWeb2022.Models
+{
+    //Modèle de vue
+    public class StudentsViewModel
+    {
+        public List<StudentsModel> lstStudents { get; set; }
+    }
+
+    //Modèle de domaine
+    public class StudentsModel
+    {
+        public StudentsModel(string nom, string prenom, string promo)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Promo = promo;
+        }
+
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Promo { get; set; }
+    }
+
+    //Notre BDD
+    public static class StudentsDatas
+    {
+        public static List<StudentsModel> allStudents = new List<StudentsModel>
+        {
+            new StudentsModel("BUTIN", "Dominique", "Full stack"),
+            new StudentsModel("DISS", "Christelle", "C# .net"),
+            new StudentsModel("DAUCE", "Laurent", "Full stack"),
+            new StudentsModel("DAUCE", "Laurent", "Web"),
+            new StudentsModel("ALPHONSE", "Rooldy", "Java"),
+            new StudentsModel("CANUEL", "Stéphanie", "C# .net"),
+            new StudentsModel("CLEMENCEY", "Yann", "C# .net")
+        };
+    }
+}
